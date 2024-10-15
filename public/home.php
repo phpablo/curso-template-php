c4652feefdca89172b673bcfc04a95285db621ed
phpablo <pablohenrique120@gmail.com>
2024-10-15 16:23:21 -0300

    :bug: add <p>

--- public/home.php
+++ public/home.php
@@ -10,6 +10,7 @@
 <body>
   <h1>Home</h1>
   <p>Olá <?= $nome ?></p>
+  <p>Não, disse Ron, em um tom nada convincente. Mas antes que Harry pudesse exigir ver o jornal. Draco Malfoy gritou do outro lado do Salão Principal, da mesa da Sonserina.Ei, Potter! Potter! Como está sua cabeça? Você está se sentindo bem? Tem certeza de que não vai enlouquecer conosco?Malfoy também estava segurando um exemplar do Profeta Diário. Os sonserinos de todos os lados da mesa estavam rindo, girando em seus assentos para ver a reação de Harry.</p>
 
 </body>
 
