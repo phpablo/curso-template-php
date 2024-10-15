b3a7ce9f8b535d6dab5a309c235c74430b836662
phpablo <pablohenrique120@gmail.com>
2024-10-15 16:14:24 -0300

    :sparkles: feat: How buffet works

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
-echo 'Home';
+
+$data = ['nome' => 'Gohan'];
+
+ob_start();
+
+extract($data);
+
+require 'home.php';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump(htmlspecialchars($content));
